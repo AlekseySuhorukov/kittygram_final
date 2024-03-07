e6ef0cc2ae40003475422773415f83b89581a85d
Artemiy Vishnevskiy <temaa.mann@gmail.com>
2024-03-08 00:38:47 +0500

    first deploy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,15 @@
 # flake8: noqa
 import os
 
-from dotenv import load_dotenv
 from pathlib import Path
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
